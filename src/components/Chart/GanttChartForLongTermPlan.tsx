e7206feefd48fa74c1f583d7088f45bcffaf5217
hyun <hyunsokstar@gmail.com>
2023-05-09 16:59:23 +0900

    update for gantt update ready

--- src/components/Chart/GanttChartForLongTermPlan.tsx
+++ src/components/Chart/GanttChartForLongTermPlan.tsx
@@ -1,24 +1,43 @@
-import React from "react";
+// LongTermPlanContent
+import React, { useState } from "react";
 import { FrappeGantt, ViewMode, Task } from "frappe-gantt-react";
 import { LongTermPlanContentList } from "../../types/type_for_plan_maker";
 
 type Props = { dataForPlanContents: LongTermPlanContentList };
 
-const GanttChartForPlanContents = ({ dataForPlanContents }: Props) => {
-  const tasks = dataForPlanContents.map((item) => {
-    return new Task({
-      id: item.id.toString(),
-      name: item.name,
-      start: item.start,
-      end: item.end,
-      progress: item.progress,
-      dependencies: item.dependencies,
-    });
-  });
+const GanttChartForPlanContents: React.FC<Props> = ({ dataForPlanContents }) => {
+  const [tasks, setTasks] = useState<Task[]>(
+    dataForPlanContents.map((item) => {
+      return new Task({
+        id: item.id.toString(),
+        name: item.name,
+        start: item.start,
+        end: item.end,
+        progress: item.progress,
+        dependencies: item.dependencies,
+      });
+    })
+  );
 
   const viewMode = ViewMode.Day; // or ViewMode.Week, ViewMode.Month
 
-  return <FrappeGantt tasks={tasks} viewMode={viewMode} />;
+  const handleTasksChange = (updatedTasks: Task[]) => {
+    console.log("updatedTasks : ", updatedTasks);
+
+    // Update the local state
+    setTasks(updatedTasks);
+
+    // Call API to update the server, if necessary
+    // ...
+  };
+
+  return (
+    <FrappeGantt
+      tasks={tasks}
+      viewMode={viewMode}
+      onTasksChange={handleTasksChange}
+    />
+  );
 };
 
 export default GanttChartForPlanContents;
