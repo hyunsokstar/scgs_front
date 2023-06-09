2c6703748e449a435de3c0b6d1a6b16ea3687368
hyun <hyunsokstar@gmail.com>
2023-06-10 03:01:05 +0900

    update

--- src/components/GridTable/Formatter/CheckBoxForGrid.tsx
+++ src/components/GridTable/Formatter/CheckBoxForGrid.tsx
@@ -1,26 +1,28 @@
 import React from "react";
-import { Column, RowRendererProps } from "react-data-grid";
+import { Column } from "react-data-grid";
+import { Checkbox } from "@chakra-ui/react";
 
 interface Props {
-    row: RowRendererProps<any>['row'];
-    onRowChange: (row: RowRendererProps<any>['row']) => void;
-    column?: Column<any>;
-    onClose?: () => void;
-  }
-  
+  row: any;
+  onRowChange: (row: any) => void;
+  column?: Column<any>;
+  onClose?: () => void;
+}
 
-const CheckBoxForGrid: React.FC<Props> = ({ row, onRowChange }) => {
+const CheckBoxForGrid = ({ row, onRowChange }: Props) => {
   return (
-    <input
-      type="checkbox"
+    <Checkbox
       value={row.id}
-      checked={row.selected}
+      isChecked={row.selected}
       onChange={(e) => {
         const checked = e.target.checked;
         const new_row = { ...row, selected: checked };
-        console.log("new_row : ", new_row);
+        console.log("new_row: ", new_row);
         onRowChange(new_row);
       }}
+      defaultChecked={row.selected}
+      onBlur={() => {}}
+      checked={row.selected}
     />
   );
 };
