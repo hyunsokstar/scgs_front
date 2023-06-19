10732cd33acf6a51faf05a3b4878bbdb0611f29d
hyun <hyunsokstar@gmail.com>
2023-06-19 14:32:41 +0900

    update for note editor

--- src/routes/Slide/NoteContentsByImageSlidePage.tsx
+++ src/routes/Slide/NoteContentsByImageSlidePage.tsx
@@ -1,14 +1,21 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+    import { Box } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import NoteSlideForStudyNoteSpecificPage from "../../components/ImageSlide/NoteSlideForStudyNoteSpecificPage";
+import TinyMCEEditor from "../../components/RichEditor/TinyMCEEditor";
 
 interface Props {}
 
 // 1122
 const NoteContentsByImageSlidePage = (props: Props) => {
   const { study_note_pk, note_page_num } = useParams();
+  const [note_content_content, set_note_content_content] = useState<string>("");
 
+  const handleChangeForNoteContent = () => {
+    console.log("hi");
+  };
+
+  // 2244
   return (
     <Box>
       <Box>note_pk: {study_note_pk}</Box>
@@ -18,7 +25,10 @@ const NoteContentsByImageSlidePage = (props: Props) => {
           <NoteSlideForStudyNoteSpecificPage />
         </Box>
         <Box width={"50%"} border={"1px solid red"}>
-          right
+          <TinyMCEEditor
+            onChange={handleChangeForNoteContent}
+            apiKey="mj1ss81rnxfcig1ol8gp6j8oui9jpkp61hw3m901pbt14ei1"
+          />
         </Box>
       </Box>
     </Box>
