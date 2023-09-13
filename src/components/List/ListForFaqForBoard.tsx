ebdea4df08874fce6d91d0229a969a09d046e520
hyun <hyunsokstar@gmail.com>
2023-09-13 17:37:00 +0900

    update for faq detail

--- src/components/List/ListForFaqForBoard.tsx
+++ src/components/List/ListForFaqForBoard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Text,
@@ -14,6 +14,7 @@ import { ITypeForFaqRow } from "../../types/board_type";
 import ModalButtonForUpdateFaqForBoard from "../modal/ModalButtonForUpdateFaqForBoard";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { apiForDeleteFaqForBoard } from "../../apis/board_api";
+import ModalForFaqDetailForBoard from "../modal/ModalForFaqDetailForBoard";
 
 interface IPropsForFaqList {
   faqList: ITypeForFaqRow[];
@@ -22,6 +23,8 @@ interface IPropsForFaqList {
 const ListForFaqForBoard: React.FC<IPropsForFaqList> = ({ faqList }) => {
   const toast = useToast();
   const queryClient = useQueryClient();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedFaq, setSelectedFaq] = useState<any>();
 
   const mutationForDeleteFaqForBoard = useMutation(
     (pk: string | number) => {
@@ -41,11 +44,9 @@ const ListForFaqForBoard: React.FC<IPropsForFaqList> = ({ faqList }) => {
           status: "success",
         });
       },
-      onError: (error:any) => {
+      onError: (error: any) => {
         console.error("FAQ 삭제 오류:", error);
-        if (
-          error.response.data.message
-        ) {
+        if (error.response.data.message) {
           toast({
             title: "FAQ 삭제 실패",
             description: error.response.data.message,
@@ -66,59 +67,81 @@ const ListForFaqForBoard: React.FC<IPropsForFaqList> = ({ faqList }) => {
     mutationForDeleteFaqForBoard.mutate(faqId);
   };
 
+  const openModal = (faq: any) => {
+    setSelectedFaq(faq);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedFaq(null);
+  };
+
   return (
-    <VStack spacing={2} align="stretch">
-      {faqList.length === 0 ? (
-        <Box p={2} textAlign="center">
-          데이터가 없습니다.
-        </Box>
-      ) : (
-        faqList.map((faq) => (
-          <Box
-            key={faq.id}
-            borderWidth="1px"
-            borderRadius="lg"
-            p={2}
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Checkbox size="sm" mr={2} />
-            <Avatar
-              name={faq.writer?.username || "User"}
-              src={faq.writer?.profile_image || ""}
-              size="sm"
-            />
-            <Text
-              flex="1"
-              pr={2}
-              fontWeight="bold"
-              color="teal.500"
-              textAlign="center"
-              _hover={{ cursor: "pointer", textDecoration: "underline" }}
-            >
-              {faq.title}
-            </Text>
-            <Spacer />
-            <Box display={"flex"} alignItems={"center"} gap={2}>
-              <ModalButtonForUpdateFaqForBoard
-                id={faq.id}
-                title={faq.title}
-                content={faq.content}
-              />
-              <IconButton
-                aria-label="삭제"
-                icon={<DeleteIcon />}
-                size="sm"
-                _hover={{ bgColor: "red.100" }}
-                variant="ghost"
-                onClick={() => handleDeleteFaq(faq.id)} // 딜리트 핸들러 함수 호출
-              />
-            </Box>
+    <Box>
+      <VStack spacing={2} align="stretch">
+        {faqList.length === 0 ? (
+          <Box p={2} textAlign="center">
+            데이터가 없습니다.
           </Box>
-        ))
-      )}
-    </VStack>
+        ) : (
+          faqList.map((faq) => (
+            <Box
+              key={faq.id}
+              borderWidth="1px"
+              borderRadius="lg"
+              p={2}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Checkbox size="sm" mr={2} />
+              <Avatar
+                name={faq.writer?.username || "User"}
+                src={faq.writer?.profile_image || ""}
+                size="sm"
+              />
+              <Text
+                flex="1"
+                pr={2}
+                fontWeight="bold"
+                color="teal.500"
+                textAlign="center"
+                _hover={{ cursor: "pointer", textDecoration: "underline" }}
+                onClick={() => openModal(faq)} // 클릭 이벤트 추가하여 모달 열기
+              >
+                {faq.title}
+              </Text>
+              <Spacer />
+              <Box display={"flex"} alignItems={"center"} gap={2}>
+                <ModalButtonForUpdateFaqForBoard
+                  id={faq.id}
+                  title={faq.title}
+                  content={faq.content}
+                />
+                <IconButton
+                  aria-label="삭제"
+                  icon={<DeleteIcon />}
+                  size="sm"
+                  _hover={{ bgColor: "red.100" }}
+                  variant="ghost"
+                  onClick={() => handleDeleteFaq(faq.id)} // 딜리트 핸들러 함수 호출
+                />
+              </Box>
+            </Box>
+          ))
+        )}
+      </VStack>
+
+      <ModalForFaqDetailForBoard
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        faqId={selectedFaq?.pk}
+        title={selectedFaq?.title || ""}
+        content={selectedFaq?.content || ""}
+      />
+
+    </Box>
   );
 };
 
---
+++ src/components/modal/ModalForFaqDetailForBoard.tsx
@@ -0,0 +1,161 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  useToast,
+} from "@chakra-ui/react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+interface Suggestion {
+  pk: number;
+  title: string;
+  writer: {
+    username: string;
+    profile_image: string | null;
+  };
+  created_at_formatted: string;
+  content: string | ""; // 내용 추가
+}
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  content: string;
+  faqId: string | number;
+}
+
+// 1122
+const ModalForFaqDetailForBoard: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  faqId,
+  title,
+  content
+}) => {
+  const toast = useToast();
+  const queryClient = useQueryClient();
+  const [comment, setComment] = useState("");
+
+  //   const {
+  //     isLoading: isLoadingForGetComment,
+  //     data: commentData,
+  //     refetch: refetchForGetCommentData,
+  //   } = useQuery<any>(
+  //     ["apiForGetCommentListForSuggestion", suggestionPk],
+  //     apiForGetCommentListForSuggestionForNote,
+  //     {
+  //       enabled: !!suggestionPk, // suggestionPk가 존재할 때만 쿼리를 활성화
+  //       // cacheTime: 0, // 캐싱 비활성화
+  //     }
+  //   );
+
+  //   console.log("commentData : ", commentData);
+
+  const handleCommentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // 댓글 내용이 변경될 때 상태 업데이트
+    setComment(e.target.value);
+  };
+
+  // mutationForUpdateForCommentForSuggestionForBoard
+  //   const mutationForAddCommentForSuggestionForNote = useMutation(
+  //     apiForAddCommentForSuggestionForNote,
+  //     {
+  //       onMutate: () => {
+  //         console.log("mutation starting");
+  //       },
+  //       onSuccess: (data) => {
+  //         console.log("data : ", data);
+
+  //         toast({
+  //           title: "comment 추가 !!",
+  //           description: data.message,
+  //           status: "success",
+  //           duration: 2000,
+  //           isClosable: true,
+  //         });
+  //         queryClient.refetchQueries(["apiForGetCommentListForSuggestion"]);
+  //       },
+  //       onError: (error: any) => {
+  //         console.log("error.response : ", error);
+  //         console.log("mutation has an error", error.response.data);
+  //       },
+  //     }
+  //   );
+
+  const handleSubmitComment = () => {
+    // 댓글을 제출하는 함수
+    if (comment.trim() === "") {
+      alert("댓글 내용을 입력하세요.");
+      return;
+    }
+    // alert(comment)
+    // mutationForAddCommentForSuggestionForNote.mutate({
+    //   suggestionPk: suggestion.pk,
+    //   content: comment,
+    // });
+    setComment("");
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // 엔터 키를 눌렀을 때만 처리
+    if (e.key === "Enter") {
+      handleSubmitComment();
+    }
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size="full">
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody style={{ height: "40%" }}>
+          <Box>
+            <Box
+              dangerouslySetInnerHTML={{ __html: content }}
+              style={{ minHeight: "200px", border: "1px solid gray" }}
+            ></Box>
+            {/* 댓글 추가 인풋 */}
+            <InputGroup mt={4}>
+              <Input
+                placeholder="댓글을 입력하세요..."
+                //   value={comment}
+                onChange={handleCommentChange}
+                onKeyDown={handleKeyPress}
+              />
+              <InputRightElement width="auto" mr={1}>
+                <Button
+                  colorScheme="blue"
+                  size="sm"
+                  onClick={handleSubmitComment}
+                >
+                  댓글 추가
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+
+            {/* <CommentListForSuggestion commentList={commentData?.comments} /> */}
+          </Box>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            닫기
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ModalForFaqDetailForBoard;
