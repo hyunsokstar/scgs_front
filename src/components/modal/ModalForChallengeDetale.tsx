047a52848a29e089a3fc4eb994fa737f1c601773
hyun <hyunsokstar@gmail.com>
2023-09-14 17:34:59 +0900

    update for challenge

--- src/components/modal/ModalForChallengeDetale.tsx
+++ src/components/modal/ModalForChallengeDetale.tsx
@@ -9,16 +9,12 @@ import {
   ModalHeader,
   ModalOverlay,
   Text,
-  VStack, // 추가
-  Divider, // 추가
-  Textarea, // 추가
+  HStack,
+  Divider,
 } from "@chakra-ui/react";
-import ContainerForCommentForSuggestion from "../Container/ContainerForCommentForSuggestionForBoard";
+import ContainerForCommentForSuggestionForBoard from "../Container/ContainerForCommentForSuggestionForBoard";
 
-const ModalForChallengeDetale: React.FC<any> = ({
-  isOpen,
-  onClose,
-}) => {
+const ModalForChallengeDetale: React.FC<any> = ({ isOpen, onClose }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="full">
       <ModalOverlay />
@@ -26,7 +22,32 @@ const ModalForChallengeDetale: React.FC<any> = ({
         <ModalHeader>challengeDetail</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          challenge detail body
+          <HStack spacing={4}>
+            {/* 왼쪽 영역 (드래그 앤 드롭 업로드 영역) */}
+            <Box
+              w="30%" // 30% 너비로 변경
+              border="2px dashed #ccc"
+              p={2}
+              margin={2}
+              borderColor={"orange.500"}
+            >
+              {/* 드래그 앤 드롭 업로드 영역 내용 */}
+              드래그 앤 드롭 업로드 영역입니다.
+            </Box>
+            {/* 가운데 구분선 */}
+            <Divider orientation="vertical" borderColor="gray.300" />
+            {/* 오른쪽 영역 (challenge detail) */}
+            <Box
+              w="70%" // 70% 너비로 변경
+              border="1px solid #ccc"
+              p={4}
+              margin={2}
+              borderColor={"gray.300"}
+            >
+              {/* challenge detail 내용 */}
+              challenge detail 내용입니다.
+            </Box>
+          </HStack>
         </ModalBody>
       </ModalContent>
     </Modal>
@@ -34,4 +55,3 @@ const ModalForChallengeDetale: React.FC<any> = ({
 };
 
 export default ModalForChallengeDetale;
-
