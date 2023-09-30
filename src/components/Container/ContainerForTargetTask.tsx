a65061d13d897eccb694918695c94353e89ec5b3
hyun <hyunsokstar@gmail.com>
2023-09-30 20:20:50 +0900

    update for task intergration

--- src/components/Container/ContainerForTargetTask.tsx
+++ src/components/Container/ContainerForTargetTask.tsx
@@ -1,5 +1,21 @@
 import React, { useState } from "react";
-import { Box, Text, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { apiForGetTaskListForTaskIntegration } from "../../apis/project_progress_api";
 import { IDataForTaskListForIntegration } from "../../types/project_progress/project_progress_type";
@@ -8,6 +24,9 @@ interface Props {}
 
 const ContainerForTargetTask = (props: Props) => {
   const [pageNum, setPageNum] = useState(1);
+  const [selectedPk, setSelectedPk] = useState(null);
+  const [selectedTask, setSelectedTask] = useState(""); // 추가: 선택한 행의 title을 관리
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const {
     isLoading,
@@ -21,7 +40,26 @@ const ContainerForTargetTask = (props: Props) => {
     }
   );
 
-  console.log("dataForTaskListForCheckedPks : ", dataForTaskListForCheckedPks);
+  const handleRowSelect = (rowId: any, rowTask: string) => {
+    setSelectedPk(rowId);
+
+    // 모달 열기
+    setIsModalOpen(true);
+    // alert(rowTask);
+    // 선택한 행의 title 설정
+    setSelectedTask(rowTask);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleConfirm = () => {
+    // 여기에서 선택된 행(row.task)의 부가 업무로 전환하는 작업을 수행할 수 있습니다.
+    // 이 부분은 실제로 구현해야 합니다.
+    // 작업이 완료되면 모달을 닫을 수 있습니다.
+    closeModal();
+  };
 
   return (
     <Box>
@@ -41,11 +79,23 @@ const ContainerForTargetTask = (props: Props) => {
         <Tbody>
           {dataForTaskListForCheckedPks ? (
             dataForTaskListForCheckedPks.listForTask.map((row) => (
-              <Tr key={row.id}>
+              <Tr
+                key={row.id}
+                // 선택된 행의 배경색 변경
+                bg={row.id === selectedPk ? "blue.50" : "transparent"}
+              >
                 <Td>{row.task_manager.username}</Td>
                 <Td>{row.task}</Td>
                 <Td>{row.current_status}</Td>
-                <Td>선택</Td>
+                <Td>
+                  {/* 선택 버튼 클릭 시 해당 행의 id와 title 저장 */}
+                  <Button
+                    size="sm"
+                    onClick={() => handleRowSelect(row.id, row.task)}
+                  >
+                    선택
+                  </Button>
+                </Td>
               </Tr>
             ))
           ) : (
@@ -55,6 +105,30 @@ const ContainerForTargetTask = (props: Props) => {
           )}
         </Tbody>
       </Table>
+
+      {/* 모달 */}
+      <Modal isOpen={isModalOpen} onClose={closeModal} size="5xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>안내 메세지</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {selectedTask ? (
+              <p>{`체크한 업무들을 선택한 행의 "${selectedTask}"의 부가 업무로 전환하시겠습니까?`}</p>
+            ) : (
+              <p>선택한 업무가 없습니다.</p>
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleConfirm}>
+              확인
+            </Button>
+            <Button colorScheme="gray" onClick={closeModal}>
+              취소
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
