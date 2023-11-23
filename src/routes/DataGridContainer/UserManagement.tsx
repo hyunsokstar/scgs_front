c2ece302be276f3eb239105a42bde87f0e36917c
hyunsokstar <hyunsokstar@gmail.com>
2023-11-23 21:40:27 +0900

    update for selected rows

--- src/routes/DataGridContainer/UserManagement.tsx
+++ src/routes/DataGridContainer/UserManagement.tsx
@@ -1,21 +1,61 @@
 import React, { useState } from "react";
-import ReactDataGrid, {
+import DataGrid, {
   Column,
   RenderCellProps,
   RenderEditCellProps,
   RenderCheckboxProps,
   RenderHeaderCellProps,
+  RenderGroupCellProps,
   useRowSelection,
 } from "react-data-grid";
 import { Box, Input } from "@chakra-ui/react";
 
-function SelectFormatter(props: RenderCellProps<any>) {
+export function renderCheckbox({ onChange, ...props }: RenderCheckboxProps) {
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    console.log("props : ", props);
-    console.log("id : ", props.row.id);
+    onChange(e.target.checked, (e.nativeEvent as MouseEvent).shiftKey);
   }
 
   return <input type="checkbox" {...props} onChange={handleChange} />;
+}
+
+function SelectCellFormatter({
+  value,
+  tabIndex,
+  disabled,
+  onChange,
+  "aria-label": ariaLabel,
+  "aria-labelledby": ariaLabelledBy,
+}: SelectCellFormatterProps) {
+  // const renderCheckbox = useDefaultRenderers()!.renderCheckbox!;
+
+  return renderCheckbox({
+    "aria-label": ariaLabel,
+    "aria-labelledby": ariaLabelledBy,
+    tabIndex,
+    disabled,
+    checked: value,
+    onChange,
+  });
+}
+
+function SelectFormatter(props: RenderCellProps<unknown>) {
+  const [isRowSelected, onRowSelectionChange] = useRowSelection();
+
+  return (
+    <SelectCellFormatter
+      aria-label="Select"
+      tabIndex={props.tabIndex}
+      value={isRowSelected}
+      onChange={(checked, isShiftClick) => {
+        onRowSelectionChange({
+          type: "ROW",
+          row: props.row,
+          checked,
+          isShiftClick,
+        });
+      }}
+    />
+  );
 }
 
 function autoFocusAndSelect(input: HTMLInputElement | null) {
@@ -41,24 +81,72 @@ function textEditor<TRow, TSummaryRow>({
   );
 }
 
-function UserManagement() {
-  const SelectColumn: Column<any, any> = {
-    key: "selected",
-    name: "",
-    width: 35,
-    minWidth: 35,
-    maxWidth: 35,
-    resizable: false,
-    sortable: false,
-    frozen: true,
-    // renderHeaderCell(props) {
-    //   return <HeaderRenderer {...props} />;
-    // },
-    renderCell(props: any) {
-      return <SelectFormatter {...props} />;
-    },
-  };
+type SharedInputProps = Pick<
+  RenderCheckboxProps,
+  "disabled" | "tabIndex" | "aria-label" | "aria-labelledby"
+>;
 
+interface SelectCellFormatterProps extends SharedInputProps {
+  value: boolean;
+  onChange: (value: boolean, isShiftClick: boolean) => void;
+}
+
+function HeaderRenderer(props: RenderHeaderCellProps<unknown>) {
+  const [isRowSelected, onRowSelectionChange] = useRowSelection();
+
+  return (
+    <SelectCellFormatter
+      aria-label="Select All"
+      tabIndex={props.tabIndex}
+      value={isRowSelected}
+      onChange={(checked) => {
+        onRowSelectionChange({ type: "HEADER", checked });
+      }}
+    />
+  );
+}
+
+function SelectGroupFormatter(props: RenderGroupCellProps<unknown>) {
+  const [isRowSelected, onRowSelectionChange] = useRowSelection();
+
+  return (
+    <SelectCellFormatter
+      aria-label="Select Group"
+      tabIndex={props.tabIndex}
+      value={isRowSelected}
+      onChange={(checked) => {
+        onRowSelectionChange({
+          type: "ROW",
+          row: props.row,
+          checked,
+          isShiftClick: false,
+        });
+      }}
+    />
+  );
+}
+
+const SelectColumn: Column<any, any> = {
+  key: "selected",
+  name: "",
+  width: 35,
+  minWidth: 35,
+  maxWidth: 35,
+  resizable: false,
+  sortable: false,
+  frozen: true,
+  renderHeaderCell(props) {
+    return <HeaderRenderer {...props} />;
+  },
+  renderCell(props: any) {
+    return <SelectFormatter {...props} />;
+  },
+  // renderGroupCell(props) {
+  //   return <SelectGroupFormatter {...props} />;
+  // },
+};
+
+function UserManagement() {
   const columns = [
     SelectColumn,
     { key: "id", name: "ID" },
@@ -79,9 +167,32 @@ function UserManagement() {
   ];
 
   const [rows, setRows] = useState(rowsData);
-  const [checkedIds, setCheckedIds] = useState<number[]>([]);
+  const [selectedRows, setSelectedRows] = useState(
+    (): ReadonlySet<number> => new Set()
+  );
 
-  return <ReactDataGrid columns={columns} rows={rows} onRowsChange={setRows} />;
+  function rowKeyGetter(row: any) {
+    return row.id;
+  }
+
+  // 선택된 행의 ID를 배열로 얻기 위해
+  const selectedRowIds = Array.from(selectedRows);
+
+  return (
+    <Box>
+      {/* 선택된 행의 ID들 출력 */}
+      <div>Selected Row IDs: {selectedRowIds.join(", ")}</div>
+
+      <DataGrid
+        rowKeyGetter={rowKeyGetter}
+        columns={columns}
+        rows={rows}
+        selectedRows={selectedRows}
+        onSelectedRowsChange={setSelectedRows}
+        onRowsChange={setRows}
+      />
+    </Box>
+  );
 }
 
 export default UserManagement;
